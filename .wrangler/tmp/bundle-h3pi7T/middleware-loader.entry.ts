9bff9cb78c6527968833441a1a45b4a6b3f6d7f3
Marco <clamafloro@gmail.com>
2025-12-15 03:00:02 +0100

    Quick update - 15/12/2025  3:00:02,41

--- .wrangler/tmp/bundle-h3pi7T/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-h3pi7T/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\Marco\\SSD 2 TB (C)\\Codici\\Sitopdp\\site\\.wrangler\\tmp\\bundle-3KbhJf\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\Marco\\SSD 2 TB (C)\\Codici\\Sitopdp\\site\\.wrangler\\tmp\\bundle-h3pi7T\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\Marco\\SSD 2 TB (C)\\Codici\\Sitopdp\\site\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\Marco\\SSD 2 TB (C)\\Codici\\Sitopdp\\site\\.wrangler\\tmp\\bundle-3KbhJf\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\Marco\\SSD 2 TB (C)\\Codici\\Sitopdp\\site\\.wrangler\\tmp\\bundle-h3pi7T\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\Marco\\SSD 2 TB (C)\\Codici\\Sitopdp\\site\\.wrangler\\tmp\\bundle-3KbhJf\\middleware-insertion-facade.js";
+export * from "C:\\Users\\Marco\\SSD 2 TB (C)\\Codici\\Sitopdp\\site\\.wrangler\\tmp\\bundle-h3pi7T\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
