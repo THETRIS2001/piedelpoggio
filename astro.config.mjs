d8a6d9aa435b44c127a765c7adae7c1407526b2f
Marco <clamafloro@gmail.com>
2025-12-15 02:56:38 +0100

    Use Cloudflare D1 for bookings and update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
   site: 'https://piedelpoggio.org',
@@ -24,11 +25,6 @@ export default defineConfig({
     server: {
       hmr: {
         overlay: true
-      }
-    },
-    resolve: {
-      alias: {
-        'react-dom/server': 'react-dom/server.edge'
       }
     }
   }
